bc529135c2186ca40cf349caada107d502c396fc
yurikbv <yurikbv@yandex.ru>
2018-01-25 04:07:00 +0300

    modofoed

--- app/js/common.js
+++ app/js/common.js
@@ -69,7 +69,25 @@ $(function() {
     var ths = $(this);
     ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
   });
+
   $('select').selectize({
     create: true
-  })
+  });
+
+  //E-mail Ajax Send
+  $("form.callback").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      $(th).find('.success').addClass('active').css('dispaly','flex').hide().fadeIn();
+      setTimeout(function() {
+        $(th).find('.success').removeClass('active').fadeOut();
+        th.trigger("reset");
+      }, 3000);
+    });
+    return false;
+  });
 });
